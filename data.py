cc62d50536f919cb5b2d8160c2b33b4a19087bda
Aykan <47181213+aykityy@users.noreply.github.com>
2022-11-23 05:20:34 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,33 @@
+question_data = {"category": "Entertainment: Video Games",
+                 "type": "boolean", "difficulty": "easy",
+                 "question": "Peter Molyneux was the founder of Bullfrog Productions.",
+                 "correct_answer": "True",
+                 "incorrect_answers": ["False"]}, {"category": "Entertainment: Video Games", "type": "boolean",
+                                                   "difficulty": "easy",
+                                                   "question": "The main character in the 'Half-Life' franchise is named Morgan Freeman.",
+                                                   "correct_answer": "False", "incorrect_answers": ["True"]}, {
+                    "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                    "question": "The Mann Co. Store from Team Fortress 2 has the slogan 'We hire mercenaries and get in fights'.",
+                    "correct_answer": "False", "incorrect_answers": ["True"]}, {
+                    "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                    "question": "Deus Ex (2000) does not feature the World Trade Center because it was destroyed by terrorist attacks according to the game&#039;s plot.",
+                    "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Science: Computers",
+                                                                                "type": "boolean",
+                                                                                "difficulty": "medium",
+                                                                                "question": "AMD created the first consumer 64-bit processor.",
+                                                                                "correct_answer": "True",
+                                                                                "incorrect_answers": ["False"]}, {
+                    "category": "Geography", "type": "boolean", "difficulty": "easy",
+                    "question": "California is larger than Japan.", "correct_answer": "True",
+                    "incorrect_answers": ["False"]}, {"category": "Entertainment: Video Games", "type": "boolean",
+                                                      "difficulty": "easy",
+                                                      "question": "The Konami Code is known as Up, Up, Down, Down, Left, Right, Right, Left, B, A, Start.",
+                                                      "correct_answer": "False", "incorrect_answers": ["True"]}, {
+                    "category": "Geography", "type": "boolean", "difficulty": "easy",
+                    "question": "Tokyo is the capital of Japan.", "correct_answer": "True",
+                    "incorrect_answers": ["False"]}, {"category": "Geography", "type": "boolean", "difficulty": "hard",
+                                                      "question": "The two largest ethnic groups of Belgium are Flemish and Walloon. ",
+                                                      "correct_answer": "True", "incorrect_answers": ["False"]}, {
+                    "category": "Science: Mathematics", "type": "boolean", "difficulty": "easy",
+                    "question": "An equilateral triangle always has every angle measuring 60&deg;.",
+                    "correct_answer": "True", "incorrect_answers": ["False"]}
